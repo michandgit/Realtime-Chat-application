93a8b8bc231d385941ef01bd0f7263cb4c30e92f
michandgit <chandsigola@gmail.com>
2025-05-04 15:18:50 +0530

    added

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,7 +8,8 @@ export const generatetoken = (userId , res) =>{
     res.cookie("jwt" , token , {
         maxAge : 7*24*60*60*1000,
         httpOnly:true,
-        sameSite:"strict",
+        secure: true,
+        sameSite:"None",
         secure:process.env.NODE_ENV !== "development"
     })
 
