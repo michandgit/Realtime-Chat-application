855397e7944991af4139c90b29124f3ffb1088bb
michandgit <chandsigola@gmail.com>
2025-05-04 15:25:22 +0530

    added

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,8 +9,7 @@ export const generatetoken = (userId , res) =>{
         maxAge : 7*24*60*60*1000,
         httpOnly:true,
         secure: true,
-        sameSite:"None",
-        secure:process.env.NODE_ENV !== "development"
+        sameSite:"None"
     })
 
     return token;
