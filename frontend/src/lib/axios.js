f5b3deda9ebbdc676eb45541b86cdfb98878fc39
Chandsi <111673613+michandgit@users.noreply.github.com>
2025-05-04 12:10:23 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const devEnv = process.env.REACT_APP_MODE !== 'production';
 
 const axiosInstance = axios.create({
-  baseURL: devEnv ? 'http://localhost:5000/api' : 'https://your-production-url/api',
+  baseURL: devEnv ? 'http://localhost:5000/api' : 'https://realtime-chat-application-yl4i.onrender.com/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
